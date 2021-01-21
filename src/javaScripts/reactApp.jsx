987fe6aaf9668d1e4de7e7a161044170270b4c78
keita <68856311+K-Keita@users.noreply.github.com>
2021-01-21 13:36:39 +0900

    Install TypeScript

---
+++ src/javaScripts/reactApp.jsx
@@ -0,0 +1,19 @@
+import ReactDom from 'react-dom';
+import React from 'react'
+import Alert from './Alert.tsx';
+
+const App = (props) => {
+  return (
+    <div style={{color: "#000"}}>
+      Hello, React App!
+      <Alert message="Success!"/>
+    </div>
+  )
+};
+
+const reactRoot = document.getElementById("react-root");
+if(reactRoot) {
+  ReactDom.render(<App/>, reactRoot);
+} else {
+  console.log("No root element found");
+}
